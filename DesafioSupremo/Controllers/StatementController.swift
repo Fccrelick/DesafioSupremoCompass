25f74b56499bdc9c89f083cbb25d3f4bf3eedeff
Fernando Crelick <fccrelick@protonmail.com>
2022-07-14 17:40:40 -0300

    removed dead code

--- DesafioSupremo/Controllers/StatementController.swift
+++ DesafioSupremo/Controllers/StatementController.swift
@@ -178,25 +178,3 @@ extension StatementController: UIScrollViewDelegate {
         }
     }
 }
-
-class FooterView: UIView {
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-
-        backgroundColor = ColorPalette.white
-    }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
-
-extension FooterView: ViewCoding {
-    func buildViewHierarchy() {
-        
-    }
-
-    func setupConstraints() {
-        
-    }
-}
